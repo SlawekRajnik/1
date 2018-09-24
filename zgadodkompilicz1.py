0d99df7e688b2a1f418254295936ec5d60328c56
SlawekRajnik <43469757+SlawekRajnik@users.noreply.github.com>
2018-09-24 14:28:41 +0200

    Add files via upload

--- zgadodkompilicz1.py
+++ zgadodkompilicz1.py
@@ -11,6 +11,9 @@ correct = word
 
 liczbal = len(correct)
 
+proba = 1
+
+
 # let's play a game
 
 print("\t///TEST AREA///", correct, "\n\n\n")
@@ -22,14 +25,41 @@ print("Zaczynajmy!")
 print("\nLiczba liter w zgadywaniem słowie to: ", liczbal, "liter")
 litera = input("A teraz spróbuj zgadnąć literę znajdującą się w tym słowie: ")
 
-#sprawdź czy użytkownik nie oszukuje
-oszust = len(litera)
 
-while oszust == 1 and proba == 5:
-    for letter in correct:
-        
-    
+liczbaliter = len(litera)
+
+while litera != "" and liczbaliter < 2:
+
+    if litera in correct and proba < 5:
+        print("TAK", proba)
+        litera = input("A teraz spróbuj zgadnąć literę znajdującą się w tym słowie: ")
+        liczbaliter = len(litera)
+        proba += 1
+    if litera not in correct and proba < 5:
+        print("NIE", proba)
+        litera = input("A teraz spróbuj zgadnąć literę znajdującą się w tym słowie: ")
+        liczbaliter = len(litera)
+        proba += 1
+    elif proba == 5:
+        if litera in correct:
+            print("TAK")
+        if litera not in correct:
+            print("NIE")
+        break
+
+else:
+    print("Oszukujesz, więc teraz zgaduj od razu :)))")
+
+#druga pętla
+
+zgaduj = input("Ostateczna próba, zgaduj słowo! Twoja odpowiedź to: ")
+
+while zgaduj != correct and zgaduj !="":
+    print("źle, jeszcze raz!")
+    zgaduj = input("Ostateczna próba, zgaduj słowo! Twoja odpowiedź to: ")
+if zgaduj == correct:
+    print("GRATULACJE ZGADŁEŚ!!! ---===KONIEC===---")
 
 
-input("To koniec programu na zaliczenie. Dziękuję!")
+input("\n\n\nTo koniec programu na zaliczenie. Dziękuję!")
 
